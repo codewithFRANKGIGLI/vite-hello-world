82492acc36200849f3c6e8cad3d970cd7b25e2e4
codewithFRANKGIGLI <gigli.francesco099@gmail.com>
2024-04-14 20:02:09 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <main>
+    <AppHero></AppHero>
+    <AppTitle></AppTitle>
+  </main>
+</template>
+
+<script>
+import AppTitle from './components/AppTitle.vue';
+import AppHero from './components/AppHero.vue';
+
+export default {
+  components: {
+    AppTitle,
+    AppHero,
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,23 @@
+<script>
+    export default {
+        name: "AppHero"
+    }
+</script>
+
+<template>
+    <div>
+        <img src="../assets/img/vite-logo.png" alt="">
+    </div>
+</template>
+
+<style scoped>
+    img {
+        width: 20%;
+        padding: 10px 0;
+    }
+    div {
+        background-color: #1e293b;
+        display: flex;
+        justify-content: center;
+    }
+</style>
